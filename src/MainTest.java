d488aced5fb9b6621da1187fd7ba690e347d4d6e
Eleonore Khachaturova <murrast@mail.ru>
2022-11-23 14:26:30 +0100

    Blyadstvo

--- src/MainTest.java
+++ src/MainTest.java
@@ -2,4 +2,6 @@ public class MainTest {
     public static void main(String[] args){
         System.out.println("prout");
     }
+    public static void blyadstvo (String[] args){System.out.println("Блядство");}
+
 }
