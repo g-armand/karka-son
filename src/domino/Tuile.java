a0a093bf55174eb5d5de2d1acb29675304b114f5
eleonorex <murrast@mail.ru>
2022-11-30 15:15:54 +0100

    added rotation

--- src/domino/Tuile.java
+++ src/domino/Tuile.java
@@ -1,11 +1,13 @@
 package domino;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
 
 public class Tuile {
 
     char[][] content = new char [5][5];
+    int n = 3; // nombre de rangs
 
     public Tuile(){
         Random a = new Random();
@@ -29,10 +31,44 @@ public class Tuile {
 
     public void spin(boolean droite){
     }
+// on a besoin de ça?
 
 
     //not good because cannot chain multiple Tuile on a same line
     public String toString(){
         return "";
+    }
+
+    public void tourner_droite(Tuile tuile, int n, String rotation){
+
+        // TRANSPOSE
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                char temp = tuile.content[i][j];
+                tuile.content[i][j] = tuile.content[j][i];
+                tuile.content[j][i] = temp;
+            }
+        }
+
+        //REVERSE ROWS
+        for (int i = 0; i < n; i++) {
+            int low = 0, high = n - 1;
+            while (low < high) {
+                if (Objects.equals(rotation, "droite")){
+                    char temp = tuile.content[i][low];
+                    tuile.content[i][low] = tuile.content[i][high];
+                    tuile.content[i][high] = temp;
+                    low++;
+                    high--;
+                }
+                if (Objects.equals(rotation, "gauche")){
+                    char temp = tuile.content[low][i];
+                    tuile.content[low][i] = tuile.content[high][i];
+                    tuile.content[high][i] = temp;
+                    low++;
+                    high--;
+                }
+            }
+        }
     }
 }
